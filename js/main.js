2b4e3d6076d9d68ff9a4c4af13e7b0bb591439a3
Boris <Borismulder@outlook.com>
2020-07-22 23:33:14 +0200

    stop cache attempt 2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
     setTimeout(function(){
         $.get("https://cat-fact.herokuapp.com/facts/random", function(data, textStatus, jqXHR){
-        var _html = data.text+'<br>'+'<img src="https://cataas.com/cat/gif?dummy=371662" alt="404 meow">';
-        $("#place_data").html(_html);
+        var number = 1 + Math.floor(Math.random() * 2048);
+        //var _html = data.text+'<br>'+'<img src="https://cataas.com/cat/gif?dummy=371662" alt="404 meow">';
+        var url = data.text+'<br>'+'<img src="https://cataas.com/cat/gif?dummy="';
+
+        $("#place_data").html(url.concat(), number ,'" alt="404 meow">');
     })
     },5000);
 })
