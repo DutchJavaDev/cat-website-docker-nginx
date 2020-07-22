1612f85738370e99c30f97f5dd99c5bf7f754937
Boris <Borismulder@outlook.com>
2020-07-22 23:13:58 +0200

    stop image cache

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     setTimeout(function(){
         $.get("https://cat-fact.herokuapp.com/facts/random", function(data, textStatus, jqXHR){
-        var _html = data.text+'<br>'+'<img src="https://cataas.com/cat/gif" alt="404 meow">';
+        var _html = data.text+'<br>'+'<img src="https://cataas.com/cat/gif?dummy=371662" alt="404 meow">';
         $("#place_data").html(_html);
     })
     },5000);
