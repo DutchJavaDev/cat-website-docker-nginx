4fb853032760a584833248cd2544a1b4eb7cb5f6
Boris <Borismulder@outlook.com>
2020-07-23 00:03:26 +0200

    done

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
-    setTimeout(function(){
-        $.get("https://cat-fact.herokuapp.com/facts/random", function(data, textStatus, jqXHR){
-        var number = 1 + Math.floor(Math.random() * 2048);
-        //var _html = data.text+'<br>'+'<img src="https://cataas.com/cat/gif?dummy=371662" alt="404 meow">';
-        var url = data.text+'<br>'+'<img src="https://cataas.com/cat/gif?dummy="';
 
-        $("#place_data").html(url.concat(number ,'" alt="404 meow">'));
-    })
-    },5000);
+    setInterval(Meow,10000);
+
+    function Meow()
+    {
+            $.get("https://cat-fact.herokuapp.com/facts/random", function(data, textStatus, jqXHR){
+                var number = 1 + Math.floor(Math.random() * 2048);
+                var url = data.text+'<br>'+'<img src="https://cataas.com/cat/gif?do_not_edit_this_query=';
+                $("#place_data").html(url+number+'" alt="404 told you not to edit it it">');
+            });
+     }
+
 })
