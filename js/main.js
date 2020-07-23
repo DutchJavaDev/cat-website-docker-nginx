75ff5c7eb0a869ae7ef3917128cdf7f2f53e1197
Boris <Borismulder@outlook.com>
2020-07-23 19:09:53 +0200

    minor fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+    Meow();
+
     setInterval(Meow,10000);
 
     function Meow()
@@ -7,7 +9,7 @@ $(document).ready(function(){
             $.get("https://cat-fact.herokuapp.com/facts/random", function(data, textStatus, jqXHR){
                 var number = 1 + Math.floor(Math.random() * 2048);
                 var url = data.text+'<br>'+'<img src="https://cataas.com/cat/gif?do_not_edit_this_query=';
-                $("#place_data").html(url+number+'" alt="404 told you not to edit it it">');
+                $("#place_data").html(url+number+'" alt="404 told you not to edit it">');
             });
      }
 
